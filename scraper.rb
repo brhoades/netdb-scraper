420f57dab84abd9fe2561cf9224ab1436f7bad5a
Billy Rhoades <billyarhoades@gmail.com>
2015-04-21 14:48:46 -0500

    Now properly retrieves and allows selection

--- scraper.rb
+++ scraper.rb
@@ -2,10 +2,65 @@ require "mechanize"
 require "nokogiri"
 require 'highline/import'
 
+def choose_type machines
+  i = 0
+  itokey = { }
+  print "Choose a type of machine:\n"
+  machines.keys.each do |key|
+    i += 1
+    print i.to_s + ") " + key.to_s + "\n"
+    itokey[i] = key
+  end
+  print "\n"
+  choice = 0
+  while choice < 1 or choice > i 
+    choice = ask("Choose: ").to_i
+  end
+  
+  machines[itokey[choice]] 
+end
+
+def choose_machine machines
+  i = 0
+  itoname = { }
+  print "Choose a machine:\n"
+  machines.each do |m|
+    name = m.split('.').first
+    i += 1
+    print i.to_s + ") " + name + "\n"
+    itoname[i] = m
+  end
+  choice = 0
+  while choice < 1 or choice > i 
+    choice = ask("Choose: ").to_i
+  end
+  
+  itoname[choice]
+end
+
+if ARGV.size > 0
+  creds = File.new(ARGV[0])
+  creds.each do |l|
+    split = l.split('=')
+    if split[0] == "username"
+      $user = split[1].strip
+    elsif split[0] == "password"
+      $pass = split[1].strip
+    end
+  end
+end
+
 fp = File.new("scraped.txt", "w")
 
+if $user == nil 
+  $user = ask("User")
+end
+if $pass == nil
+  $pass = ask("Pass") { |q| q = q.echo = "*" }
+end
+
 $agent = Mechanize.new { |agent| agent.user_agent_alias = "Linux Mozilla" }
-$agent.add_auth( "https://itweb.mst.edu/auth-cgi-bin/cgiwrap/netdb/search-hosts.pl", ask("User"), ask("Password") { |q| q.echo = "*" } )
+$agent.add_auth( "https://itweb.mst.edu/auth-cgi-bin/cgiwrap/netdb/search-hosts.pl", $user, $pass )
 
 def scrape_names( str, mode="bydesc" )
   url = "https://itweb.mst.edu/auth-cgi-bin/cgiwrap/netdb/search-hosts.pl?mode=" + mode + "&search=" + str 
@@ -26,9 +81,8 @@ def scrape_names( str, mode="bydesc" )
   return addr
 end
 
-ref = scrape_names "reference"
-mine = ref.concat scrape_names( "bjrq48", "byname" ) 
+machines = {}
+machines[:reference_machines] = scrape_names "REFERENCE"
+machines[:bjrq48] = scrape_names( "bjrq48", "byname" ) 
 
-p mine
-#list.each { |i| fp.write(i.text + " \n" + i.content + "\n\n") }
-
+choice = choose_machine choose_type machines
